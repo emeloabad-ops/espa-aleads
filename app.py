54838c28c78393329a762dc504b7357306dcfd20
Xavier <emeloabad@gmail.com>
2026-04-18 11:43:03 -0400

    fix indentation app.py

--- app.py
+++ app.py
@@ -75,6 +75,7 @@ def get_planes():
     })
 
 if __name__ == "__main__":
-    print("Abre en tu navegador: http://localhost:5000")
-port = int(os.environ.get("PORT", 5000))
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
